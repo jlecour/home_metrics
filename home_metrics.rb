a9a9c1a66e4edaa499bd3fb0dfd4484bce5ed54f
Jérémy Lecour <jeremy.lecour@gmail.com>
2012-01-11 23:10:55 +0100

    More REST

--- home_metrics.rb
+++ home_metrics.rb
@@ -5,16 +5,35 @@ require './lib/metric'
 STORE = {}
 
 get '/' do
+  redirect to('/metrics/new')
+end
+
+get '/metrics/new' do
   erb :new
 end
 
-post '/' do
+post '/metrics' do
   @metric = Metric.new(
     params['metric']['datetime'],
     params['metric']['kind'],
     params['metric']['value']
   )
+  logger.info "Received #{params.inspect}"
   
   STORE[@metric.id] = @metric.to_hash
+  logger.info "Stored #{@metric.id}: #{@metric.to_hash}"
   
+  @metric.to_hash.inspect
+end
+
+get '/metrics' do
+  STORE.inspect
+end
+
+get '/metrics/:id' do
+  if params[:id] && STORE.has_key?(params[:id])
+    STORE[params[:id]][:date].inspect
+  else
+    raise Sinatra::NotFound
+  end
 end
