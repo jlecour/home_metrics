920e85a64b23adad98acb52d62873b4bc27d34fa
Jérémy Lecour <jeremy.lecour@gmail.com>
2012-01-12 20:56:45 +0100

    move Metric to DataMapper

--- lib/metric.rb
+++ lib/metric.rb
@@ -1,29 +1,11 @@
+require 'dm-types/epoch_time.rb'
+
 class Metric
-
-  attr_reader :timestamp, :kind, :value
-
-  def initialize(timestamp, kind, value)
-    @timestamp = if timestamp.is_a?(Fixnum)
-      timestamp
-    elsif timestamp.is_a?(Time)
-      timestamp.to_i
-    else
-      Time.parse(timestamp).to_i
-    end
-    @kind  = kind
-    @value = value
-  end
-
-  def id
-    "#{timestamp}.#{kind}"
-  end
-
-  def to_hash
-    {
-      "timestamp" => timestamp,
-      "kind"      => kind,
-      "value"     => value
-    }
-  end
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :timestamp, EpochTime
+  property :kind, String
+  property :value, Float
 
 end
--- test/metric_test.rb
+++ test/metric_test.rb
@@ -5,71 +5,12 @@ class TestMetric < Test::Unit::TestCase
 
   def setup
     @params = {
-      :timestamp  => Time.now.to_i,
+      :timestamp  => Time.now.strftime('%Y-%m-%dT%H:%M:%S'),
       :kind  => "kind",
       :value => 1
     }
 
-    @metric = Metric.new(@params[:timestamp], @params[:kind], @params[:value])
+    @metric = Metric.new(@params)
   end
-
-  def test_initialize
-
-    assert_equal @params[:timestamp], @metric.timestamp
-    assert_equal @params[:kind], @metric.kind
-    assert_equal @params[:value], @metric.value
-
-  end
-
-  def test_id
-    expected = "#{@params[:timestamp]}.#{@params[:kind]}"
-
-    assert_equal expected, @metric.id
-
-  end
-
-  def test_to_hash
-    expected = {
-      'timestamp' => @params[:timestamp],
-      'kind'      => @params[:kind],
-      'value'     => @params[:value]
-    }
-
-    assert_equal expected, @metric.to_hash
-  end
-
-  def test_convert_time_to_integer
-    time = Time.now
-    @params[:timestamp] = time
-    
-    assert_equal Time, @params[:timestamp].class
-    
-    @metric = Metric.new(@params[:timestamp], @params[:kind], @params[:value])
-    expected = time.to_i
-
-    assert_equal expected, @metric.timestamp
-  end
-
-  def test_convert_string_to_integer
-    time = Time.now
-    @params[:timestamp] = time.iso8601
-    
-    assert_equal String, @params[:timestamp].class
-    
-    @metric = Metric.new(@params[:timestamp], @params[:kind], @params[:value])
-    expected = time.to_i
-    
-    assert_equal expected, @metric.timestamp
-  end
-
-  # def test_convert_time_to_date
-  #   @params[:date] = Time.now
-  # 
-  #   @metric = Metric.new(@params[:date], @params[:kind], @params[:value])
-  # 
-  #   expected = @params[:date].to_date
-  # 
-  #   assert_equal expected, @metric.date
-  # end
 
 end
